31ec505ccae9ac624533682f7f426fe2b01b7dfd
Cesar Camacho <chanko@gmail.com>
2012-07-13 11:54:57 -0700

    added publish date to asset

--- lib/actv/asset.rb
+++ lib/actv/asset.rb
@@ -9,7 +9,7 @@ module ACTV
   class Asset < ACTV::Identity
 
     attr_reader :assetGuid, :assetName, :assetDsc, :activityStartDate, :activityStartTime, :activityEndDate, :activityEndTime,
-      :homePageUrlAdr, :isRecurring, :contactName, :contactEmailAdr, :contactPhone, :showContact, :createdDate, :modifiedDate
+      :homePageUrlAdr, :isRecurring, :contactName, :contactEmailAdr, :contactPhone, :showContact, :publishDate, :createdDate, :modifiedDate
 
     alias id assetGuid
     alias title assetName
@@ -24,7 +24,8 @@ module ACTV
     alias contact_email contactEmailAdr
     alias contact_phone contactPhone
     alias show_contact? showContact
-    alias create_at createdDate
+    alias published_at publishDate
+    alias created_at createdDate
     alias updated_at modifiedDate
 
     def place
